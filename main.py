01288aad265c829701954456e3b1f85e6f6b477a
vis <48868398+vis2x@users.noreply.github.com>
2022-12-21 16:50:42 +0000

    Added: Write data to JSON file and refactored code

--- main.py
+++ main.py
@@ -2,53 +2,71 @@ from tkinter import *
 from tkinter import messagebox
 from random import randint, choice, shuffle
 import pyperclip
+import json
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
+
 def generatePassword():
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+               'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
     passwordList = []
 
-    lowerLetters = [choice(letters) for char in range(randint(3, 4))]
-    upperLetters = [choice(letters).upper() for char in range(randint(3, 4))]
+    letters = [choice(letters) for char in range(randint(6, 8))]
     symbols = [choice(symbols) for char in range(randint(2, 4))]
     numbers = [choice(numbers) for char in range(randint(2, 4))]
 
-    passwordList = lowerLetters + upperLetters + symbols + numbers
-    
+    passwordList = letters + symbols + numbers
+
     shuffle(passwordList)
-    
+
     password = "".join(passwordList)
     passwordEntry.insert(0, password)
     pyperclip.copy(password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
+
 def saveData():
     website = websiteEntry.get()
     email = emailEntry.get()
     password = passwordEntry.get()
-        
-    if len(website)==0 or len(email)==0 or len(password)==0:
-        messagebox.showerror(title=website, message="Error! \nOne or more fields are empty!")
+
+    enteredData = {
+        website: {
+            "email": email,
+            "password": password
+        }
+    }
+
+    if len(website) == 0 or len(email) == 0 or len(password) == 0:
+        messagebox.showerror(
+            title=website, message="Error! \nOne or more fields are empty!")
 
     else:
         isOk = messagebox.askokcancel(title=website, message=f"Details you have entered are: "
-                            f"\n\nWebsite: {website}" 
-                            f"\nEmail: {email}"
-                            f"\nPassword: {password}"
-                            f"\n\nIs this OK to save?")
-    
+                                      f"\n\nWebsite: {website}"
+                                      f"\nEmail: {email}"
+                                      f"\nPassword: {password}"
+                                      f"\n\nIs this OK to save?")
+
         if isOk:
-            dataFile = open("data.txt", "a")
-            dataFile.write(f"{website} | {email} | {password} \n")
-            dataFile.close()
+            dataFile = open("data.json", "r")
+            # Reads old data
+            data = json.load(dataFile) 
+            # Adds newly entered data
+            data.update(enteredData)
+            
+            dataFile = open("data.json", "w")
+            # Writes newly entered data + old data to file
+            json.dump(data, dataFile, indent=4)
+            
             websiteEntry.delete(0, END)
             passwordEntry.delete(0, END)
-        
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -63,7 +81,6 @@ canvas.create_image(100, 100, image=logoImg)
 canvas.grid(column=1, row=0)
 
 
-
 # Labels
 websiteLabel = Label(text="Website")
 websiteLabel.grid(column=0, row=1)
@@ -73,8 +90,6 @@ emailLabel.grid(column=0, row=2)
 
 passwordLabel = Label(text="Password")
 passwordLabel.grid(column=0, row=3)
-
-
 
 
 # Entries
@@ -90,14 +105,13 @@ passwordEntry = Entry(width=19)
 passwordEntry.grid(column=1, row=3)
 
 
-
 # Buttons
-generatePasswordBtn = Button(text="Generate Password", command=generatePassword)
+generatePasswordBtn = Button(
+    text="Generate Password", command=generatePassword)
 generatePasswordBtn.grid(column=2, row=3)
 
 addBtn = Button(text="Add", width=36, command=saveData)
 addBtn.grid(column=1, row=4, columnspan=2)
-
 
 
 window.mainloop()
